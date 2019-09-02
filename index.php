2e555576f7bdc172ef442b5612612ed9578bf794
Seba <gagor.sebastian@gmail.com>
2019-09-02 20:35:06 +0200

    playing bar styles, add icons

--- index.php
+++ index.php
@@ -17,17 +17,37 @@ if (isset($_SESSION['userLoggedIn'])){
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Music Player</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/font-awesome.min.css" />
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c9c259af04.js"></script>
+    <script src="assets/js/register.js"></script>
     <script src="main.js"></script>
 </head>
 <body>
    
     <section id="nowPlayingBarContainer">
-    
-    
-    
-    
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+                <i class="fa fa-play" aria-hidden="true"></i>
+                <i class="fa fa-pause" aria-hidden="true"></i>
+                <i class="fa fa-step-forward" aria-hidden="true"></i>
+                <i class="fa fa-step-backward" aria-hidden="true"></i>
+                <i class="fa fa-random" aria-hidden="true"></i>
+                <i class="fa fa-repeat" aria-hidden="true"></i>
+                <i class="fa fa-volume-up" aria-hidden="true"></i>
+                <i class="fa fa-volume-down" aria-hidden="true"></i>
+                <i class="fa fa-volume-off" aria-hidden="true"></i>
+            </div>
+
+            <div id="nowPlayingCenter">
+            
+            </div>
+
+            <div id="nowPlayingRight">
+            
+            </div>
+        </div>
     </section>
 
 </body>
