0dedf449cd9da865f64c76e9f27636cc8efbe393
Seba <gagor.sebastian@gmail.com>
2019-09-08 11:02:42 +0200

    mysql query to get albums from db

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php include("includes/header.php");?>
+
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+
+    <div class="gridViewConainer">
+
+    <?php 
+    
+    $album_query = mysqli_query( $con, "SELECT * FROM albums");
+
+    while( $row = mysqli_fetch_array($album_query) ){
+        echo $row['title'] . "<br>";
+    }
+    
+    ?>
+
+    </div>
+
 <?php include("includes/footer.php");?>
