01f783c2119043132ca8a537d205a5ba62efe1a1
Seba <gagor.sebastian@gmail.com>
2019-09-08 11:38:41 +0200

    style main content

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 
 <h1 class="pageHeadingBig">You Might Also Like</h1>
 
-    <div class="gridViewConainer">
+    <div class="gridViewContainer">
 
     <?php 
     
-    $album_query = mysqli_query( $con, "SELECT * FROM albums");
+    $album_query = mysqli_query( $con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
 
     while( $row = mysqli_fetch_array($album_query) ){
-        echo $row['title'] . "<br>";
+
+
+        echo '<div class="gridViewItem">
+            <img src="' . $row['artworkPath'] . '">
+
+            <div class="gridViewInfo">
+            
+            "' . $row['title'] . '"
+            
+            </div>
+        </div>';
     }
     
     ?>
