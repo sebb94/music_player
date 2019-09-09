b2765af513a47b4fe1a038c2dbad964356ac9d6f
Seba <gagor.sebastian@gmail.com>
2019-09-09 18:37:27 +0200

    get artist name

--- album.php
+++ album.php
@@ -10,7 +10,15 @@ $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
 
 $album = mysqli_fetch_array($albumQuery);
 
+$artistId = $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
+
+$artist = mysqli_fetch_array($artistQuery);
+
 echo $album['title'];
+echo "<br>";
+echo $artist['name'];
 
 
 ?>
