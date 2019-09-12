694108ae569326a75d951ed674a8d5ba2d069c14
Seba <gagor.sebastian@gmail.com>
2019-09-12 19:35:31 +0200

    time remaining format

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,27 @@
 let currentPlayList = [];
 let audioElement;
 
+function formatTime(songsSeconds){
+
+    let time = Math.round(songsSeconds);
+    let minutes = Math.floor(time / 60);
+    let seconds = time - (minutes * 60);
+    let extraZero = (seconds < 10) ? "0" : "";
+   
+    return minutes + ":" + extraZero + seconds;
+
+}
 
 function Audio(){
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
+
+    this.audio.addEventListener("canplay",function(){
+        let duration = formatTime(this.duration)
+        $('.progressTime.remaining').text(duration);
+
+    });
 
     this.setTrack = function(track){
         this.currentlyPlaying = track;
