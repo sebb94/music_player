38ef703665d5fbc3f1016c1c42c0b8a584fd550d
Seba <gagor.sebastian@gmail.com>
2019-09-14 18:43:32 +0200

    search for songs

--- search.php
+++ search.php
@@ -37,3 +37,61 @@ this.value=''; this.value=temp_value">
     });
 
 </script>
+
+<div class="tracklistContainer borderBottom">
+    <ul class="tracklist">
+
+    <?php 
+    
+    $songsQuery = mysqli_query($con, "SELECT id FROM songs WHERE title LIKE '$term%' LIMIT 10");
+
+    if(mysqli_num_rows($songsQuery) == 0){
+        echo "<span class='noResults'>No songs found matching " .  $term . "</span>";
+    }
+    
+    $songIdArray = array();
+    
+        $i = 1;
+        while( $row = mysqli_fetch_array($songsQuery)){
+
+            if( $i > 15){
+                break;
+            }
+
+            array_push($songIdArray, $row['id']);
+
+            $albumSong = new Song($con, $row['id']);
+            $albumArtist =  $albumSong->getArtist();
+
+            echo "<li class='trackListRow'>
+                <div class='trackCount'>
+                    <button class='controlButton play' title='Play button'><i class='fa fa-play-circle' aria-hidden='true' onclick='setTrack(\"" .  $albumSong->getId() . "\", tempPlayList, true)'></i></button> 
+                    <span class='trackNumber'>" . $i . "</span>
+                </div>
+
+                <div class='trackInfo'>
+                    <span class='trackName'>" . $albumSong->getTitle() . "</span>
+                    <span class='artistName'>" . $albumArtist->getName() . "</span>
+                </div>
+
+                <div class='trackOptions'>
+                    <i class='fa fa-ellipsis-h' aria-hidden='true'></i>
+                </div>
+
+                <div class='trackDuration'>
+                    <span class='duration'>" . $albumSong->getDuration() . "</span>
+                </div>
+            </li>
+            ";
+            $i++;
+        }
+    
+    ?>
+
+    <script>
+        let tempSongsIds = '<?php echo json_encode($songIdArray);?>';
+        tempPlayList = JSON.parse(tempSongsIds);
+        console.log(tempPlayList);
+    </script>
+    </ul>
+</div>
