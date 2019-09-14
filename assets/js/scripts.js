9985297de7b3c802ae07b605faf032fbc62cd388
Seba <gagor.sebastian@gmail.com>
2019-09-14 09:33:28 +0200

    next song play after previous song end

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,6 +38,10 @@ function Audio(){
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
+    this.audio.addEventListener("ended",function(){
+        nextSong();
+    });
+
     this.audio.addEventListener("canplay",function(){
         let duration = formatTime(this.duration)
         $('.progressTime.remaining').text(duration);
