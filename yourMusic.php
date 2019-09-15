5d83fdd421d5a8e96e28c494f5f249a6ae02bbde
Seba <gagor.sebastian@gmail.com>
2019-09-15 08:26:16 +0200

    create playlists page and some styles

---
+++ yourMusic.php
@@ -0,0 +1,14 @@
+<?php include("includes/includedFiles.php");?>
+<section class="playlistsContainer">
+
+    <div class="gridViewContainer">
+        <h2 >Playlists</h2>
+
+        <div class="buttonItems">
+        
+            <button class="btn btn-deafault">New Playlist</button>
+
+        </div>
+    
+    </div>
+</section>
