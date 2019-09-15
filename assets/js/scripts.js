2ad0cabac261ccf0ef9849b47f9884b00ef68209
Seba <gagor.sebastian@gmail.com>
2019-09-15 08:44:05 +0200

    ajax create playlist

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,115 +8,119 @@ let repeat = false;
 let shuffle = false;
 let timer;
 
-function openPage(url){
+function openPage(url) {
 
-    if(timer != null){
+    if (timer != null) {
         clearTimeout(timer);
     }
 
-    if(url.indexOf("?") == -1){
+    if (url.indexOf("?") == -1) {
         url = url + "?";
     }
     let encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
     console.log(encodedUrl);
     $("#mainContent").load(encodedUrl);
     $("body").scrollTop(0);
-    history.pushState(null,null,url);
+    history.pushState(null, null, url);
 
 }
-function createPlayList(username){
 
-    let alert = prompt("Please enter the name of your Playlist");
+function createPlayList() {
 
-    if (alert != null){
-        $.post("include/handlers/ajax/createPlayList.php", 
-        {name: alert,
-        username: username
-    }).done(function(){
-        opnePage("yourMusic.php");
-    });
+    let message = prompt("Please enter the name of your Playlist");
+
+    if (message != null) {
+        $.post("includes/handlers/ajax/createPlayList.php", {
+            name: message,
+            username: userLoggedIn
+        }).done(function (error) {
+            if (error != "") {
+                alert(error);
+                return;
+            }
+            openPage("yourMusic.php");
+        });
     }
 
 }
 
 
-function playFirstSong(){
+function playFirstSong() {
     setTrack(tempPlayList[0], tempPlayList, true);
 }
 
-function formatTime(songsSeconds){
+function formatTime(songsSeconds) {
 
     let time = Math.round(songsSeconds);
     let minutes = Math.floor(time / 60);
     let seconds = time - (minutes * 60);
     let extraZero = (seconds < 10) ? "0" : "";
-   
+
     return minutes + ":" + extraZero + seconds;
 
 }
-function updateProgressBar(audio){
+
+function updateProgressBar(audio) {
 
     $('.progressTime.current').text(formatTime(audio.currentTime));
     $('.progressTime.remaining').text(formatTime(audio.duration - audio.currentTime));
 
-    let progress = ( audio.currentTime / audio.duration ) * 100;
+    let progress = (audio.currentTime / audio.duration) * 100;
 
     $('.playbackBar .progress').css('width', progress + "%");
 }
 
 function updateVolumeProgressBar(audio) {
 
- 
+
     let volume = audio.volume * 100;
 
     $('.volumeBar .progress').css('width', volume + "%");
 }
 
 
-function Audio(){
+function Audio() {
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
-    this.audio.addEventListener("ended",function(){
+    this.audio.addEventListener("ended", function () {
         nextSong();
     });
 
-    this.audio.addEventListener("canplay",function(){
+    this.audio.addEventListener("canplay", function () {
         let duration = formatTime(this.duration)
         $('.progressTime.remaining').text(duration);
 
     });
     this.audio.addEventListener("timeupdate", function () {
-        
-        if(this.duration){
+
+        if (this.duration) {
             updateProgressBar(this);
         }
 
     });
 
-    this.audio.addEventListener("volumechange", function(){
+    this.audio.addEventListener("volumechange", function () {
 
         updateVolumeProgressBar(this);
 
     });
 
-    this.setTrack = function(track){
+    this.setTrack = function (track) {
         this.currentlyPlaying = track;
         this.audio.src = track.path;
     }
 
-    this.play = function(){
-       this.audio.play();
+    this.play = function () {
+        this.audio.play();
     }
 
-    this.pause = function(){
+    this.pause = function () {
         this.audio.pause();
     }
 
-    this.setTime = function(seconds){
+    this.setTime = function (seconds) {
         this.audio.currentTime = seconds;
     }
 }
-
-
