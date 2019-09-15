c4f4c6e74b0776c439c3bb9f8dddeab2d44ec0b7
Seba <gagor.sebastian@gmail.com>
2019-09-15 08:48:59 +0200

    create User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -1,9 +1,18 @@
 <?php 
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
 include("includes/config.php");
+include("includes/classes/User.php");
 include("includes/classes/Artist.php");
 include("includes/classes/Album.php");
 include("includes/classes/Song.php");
+
+    if(isset($_GET['userLoggedIn'])){
+        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    }else{
+        echo "username variable was not passed into page";
+        exti();
+    }
+
 }else{
     include("includes/header.php");
     include("includes/footer.php");
