618b73ead186c953f9316e6c18c139f4202eda79
Seba <gagor.sebastian@gmail.com>
2019-09-20 18:56:39 +0200

    create update details forms

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -19,6 +19,12 @@
             $row = mysqli_fetch_array($query);
             return $row['name'];
         }
+         public function getEmail(){
+
+            $query = mysqli_query($this->con, "SELECT email FROM users WHERE username='$this->username'");
+            $row = mysqli_fetch_array($query);
+            return $row['email'];
+        }
 
     }
 ?>
--- updateDetails.php
+++ updateDetails.php
@@ -1,4 +1,28 @@
 <?php 
 include("includes/includedFiles.php");
 ?>
-Hello!
+
+<section class="userDetails">
+
+    <div class="container borderBottom">
+        <h2>EMAILS</h2>
+        <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail();?>">
+        <span class="errorMessage">
+
+        </span>
+        <button class="button" onclick="">SAVE</button>
+    </div>
+
+    <div class="container">
+        <h2>PASSWORD</h2>
+        <input type="password" class="oldPassword" name="oldPassword" placeholder="Current password...">
+        <input type="password" class="newPasswod1" name="newPasswod1" placeholder="New password...">
+        <input type="password" class="newPasswod2" name="newPasswod2" placeholder="Confirm new password...">
+        <span class="errorMessage">
+
+        </span>
+        <button class="button" onclick="">SAVE</button>
+    </div>
+
+
+</section>
